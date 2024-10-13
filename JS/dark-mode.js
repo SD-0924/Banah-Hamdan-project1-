a73d8d2e29789d51c628fd429e080670ef1180ad
Banah hamdan <Banah.hamdan@gmail.com>
2024-10-14 02:05:07 +0300

    f

--- JS/dark-mode.js
+++ JS/dark-mode.js
@@ -1,21 +1,15 @@
-// Get the dark mode toggle button
 const darkModeToggle = document.getElementById('darkModeToggle');
 
-// Check for previously saved user preference in localStorage
 if (localStorage.getItem('darkMode') === 'enabled') {
     document.body.classList.add('dark-mode');
 }
 
-// Add click event listener to the button
 darkModeToggle.addEventListener('click', () => {
-    // Toggle the dark-mode class on the body
     document.body.classList.toggle('dark-mode');
 
-    // If dark mode is enabled, save the preference in localStorage
     if (document.body.classList.contains('dark-mode')) {
         localStorage.setItem('darkMode', 'enabled');
     } else {
-        // If dark mode is disabled, remove the preference from localStorage
         localStorage.setItem('darkMode', 'disabled');
     }
 });
